0560ab212982cc36bdd24cf8383751fb02496686
Jerry Anselmi <jerry.anselmi@gmail.com>
2018-01-30 18:18:18 -0300

    agregando la configuración sin validaciones

--- bomber/src/components/Header.js
+++ bomber/src/components/Header.js
@@ -1,16 +1,21 @@
 import React, { Component } from 'react';
 import Time from './Header/Time'
 import Mines from './Header/Mines'
+import Rows  from './Header/Rows'
+import Cols  from './Header/Cols'
+
 class Header extends Component {
     constructor(props) {
         super(props);
         this.state = {
             mines: 10,
+            rows: 10,
+            cols: 10,
             timeControl: 'stop'
         }
     }
     render() {
-      const { mines, timeControl } = this.state;
+      const { mines, rows, cols, timeControl } = this.state;
 
     return (
         <nav className="navbar navbar-inverse bg-primary fixed-top navbar-toggleable-md">
@@ -20,6 +25,21 @@ class Header extends Component {
                     <li className="nav-item active">
                         <div className="nav-link">
                             <Mines quantity={mines} />
+                        </div>
+                    </li>
+                    <li className="nav-item active">
+                        <div className="nav-link">
+                            <Rows quantity={rows} />
+                        </div>
+                    </li>
+                    <li className="nav-item active">
+                        <div className="nav-link">
+                            <Cols quantity={cols} />
+                        </div>
+                    </li>
+                    <li className="nav-item active">
+                        <div className="nav-link" data-toggle="modal" data-target="#config-modal">
+                            <i className="fa fa-cogs" aria-hidden="true"></i>
                         </div>
                     </li>
                 </ul>
