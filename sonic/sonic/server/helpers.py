bbe5c5d78a8ac293a4ce3a260008bef1868c6f01
Jerry Anselmi <jerry.anselmi@gmail.com>
2018-01-30 10:53:32 -0300

    Listo el tablero

--- sonic/sonic/server/helpers.py
+++ sonic/sonic/server/helpers.py
@@ -33,6 +33,22 @@ class Board(object):
             if self.__board[row][col] != '*':
                 self.__board[row][col] = '*'
                 added_mines = added_mines + 1
+                self.__increase_numbers(row, col)
+
+    def __increase_numbers(self, row, col):
+        row_init = row -1
+        for x in range(3):
+            col_init = col -1
+            for y in range(3):
+                if self.__can_increse(row, col, row_init, col_init):
+                    self.__board[row_init][col_init] = self.__board[row_init][col_init] + 1
+
+                col_init = col_init + 1
+            row_init = row_init + 1
+
+    def __can_increse(self, row, col, rowb, colb):
+        return rowb >= 0 and colb >= 0 and rowb < self.__rows \
+            and colb < self.__cols and self.__board[rowb][colb] != '*'
 
     def generate(self):
         self.__generate_base_board()
