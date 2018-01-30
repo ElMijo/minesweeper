c1bd4a31556f24017822fe2b89fc303537f0619e
Jerry Anselmi <jerry.anselmi@gmail.com>
2018-01-30 12:43:37 -0300

    Agregando el contador de tiempo

--- bomber/src/components/Header.js
+++ bomber/src/components/Header.js
@@ -1,10 +1,25 @@
 import React, { Component } from 'react';
-
+import Time from './Header/Time'
 class Header extends Component {
   render() {
+      const control = 'start';
     return (
-        <nav className="navbar navbar-inverse bg-primary fixed-top">
+        <nav className="navbar navbar-inverse bg-primary fixed-top navbar-toggleable-md">
             <a className="navbar-brand" href="/">Bomber</a>
+            <div className="collapse navbar-collapse">
+                <ul className="navbar-nav mr-auto">
+                    <li className="nav-item active">
+                        <a className="nav-link" href="#"></a>
+                    </li>
+                </ul>
+                <ul className="navbar-nav">
+                    <li className="nav-item">
+                        <div className="nav-link">
+                            <Time control={control} />
+                        </div>
+                    </li>
+                </ul>
+            </div>
         </nav>
     );
   }
---
+++ bomber/src/components/Header/Time.js
@@ -0,0 +1,47 @@
+import React, { Component } from 'react';
+import { PropTypes } from 'prop-types'
+
+class Time extends Component {
+    constructor(props) {
+        super(props);
+        this.counter = null;
+        this.state = {
+            counter: 0,
+            isStarted: false,
+        }
+        this[props.control]()
+    }
+
+    start() {
+        this.counter = setInterval(() => {
+            this.setState({counter: this.state.counter + 1, isStarted: true });
+        }, 1000);
+    }
+
+    stop() {
+        clearInterval(this.counter);
+        this.setState({isStarted: false });
+    }
+
+    pad(value) {
+        let val = value.toString();
+        return (val.length < 2 ? "0" : "")+val;
+    }
+
+    render() {
+        const seconds = this.state.counter % 60
+        const minutes = parseInt(this.state.counter / 60)
+
+        return (
+            <div>
+                <span id="minutes">{this.pad(minutes)}</span>:<span id="seconds">{this.pad(seconds)}</span>
+            </div>
+        );
+    }
+}
+
+Time.propTypes = {
+    control: PropTypes.oneOf(['start', 'stop']).isRequired
+};
+
+export default Time;
